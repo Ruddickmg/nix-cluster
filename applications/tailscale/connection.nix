fd65a4ab8f611b51d9a07aab93ea3655f697947a
grant <ruddickmg@gmail.com>
2025-05-04 21:37:54 -1000

    feat: add cron job to refresh tls certificates monthly

--- applications/tailscale/connection.nix
+++ applications/tailscale/connection.nix
@@ -7,6 +7,7 @@
     };
 
     config = lib.mkIf config.services.tailscale.enable {
+
         # create a oneshot job to authenticate to Tailscale
         systemd.services.tailscale-autoconnect = {
             description = "Automatic connection to Tailscale";
@@ -23,33 +24,27 @@
             script = with pkgs; ''
               # wait for tailscaled to settle
               sleep 2
-              echo "checking tailscale status"
 
               # check if we are already authenticated to tailscale
               status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
               if [ $status = "Running" ]; then # if so, then do nothing
                 exit 0
               fi
-              echo "authenticating tailscale"
 
               # otherwise authenticate with tailscale
               ${tailscale}/bin/tailscale up -authkey ${config.services.tailscale.authKey}
 
-              echo "setting up tailscale certs"
-
               # set up tls certificates
               ${tailscale}/bin/tailscale cert ${config.networking.hostName}.tailbac0df.ts.net
-
-              echo "finished setting up tailscale"
             '';
         };
-    };
 
-#    TODO: set up a cron job to refresh certs every 30 days
-#    services.cron = {
-#      enable = true;
-#      systemCronJobs = [
-#        "*/5 * * * *      root    sudo tailscale cert ${networking.hostName}.tailbac0df.ts.net"
-#      ];
-#    };
+        # refresh tls certs once a month
+        services.cron = {
+            enable = true;
+            systemCronJobs = [
+                "*/0 0 1 * *      root    sudo tailscale cert ${config.networking.hostName}.tailbac0df.ts.net"
+            ];
+        };
+    };
 }
