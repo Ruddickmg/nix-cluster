8a958cae8ae7c981510fdb0615c8b5915d5de7c9
Marcus Ruddick <Ruddickmg@gmail.com>
2025-05-04 23:55:32 -1000

    feat: add hostname to allowed urls (#4)

--- applications/tailscale/default.nix
+++ applications/tailscale/default.nix
@@ -5,17 +5,11 @@
     ];
 
     config = lib.mkIf config.services.tailscale.enable {
-
-      # make the tailscale command usable to users
         environment.systemPackages = [
             pkgs.tailscale
         ];
-
         networking.firewall = {
-            # allow traffic from your Tailscale network
             trustedInterfaces = [ "tailscale0" ];
-
-            # allow the Tailscale UDP port through the firewall
             allowedUDPPorts = [ config.services.tailscale.port ];
         };
     };
